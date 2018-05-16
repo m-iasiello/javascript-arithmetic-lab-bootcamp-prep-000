f5b70c9a811ee82022cc9c3dbc6a508948472a54
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-16 01:48:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 //add
+function add(a,b) {
+  return a + b
+}
 
 //subtract
+function subtract(a,b){
+  return a - b 
+}
 
 //multiply
+function multiply(a,b) {
+  return a * b 
+}
 
 //divide
+function divide(a,b) {
+  return a / b 
+}
 
 //increment
+function inc(n) {
+  return n++
+}
 
 //decrement
+function
 
+//make integer
+
+//make decimal
